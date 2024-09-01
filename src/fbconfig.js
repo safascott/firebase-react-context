b512b5ee70779ae124f2accef997fef1f18a6366
Safa Scott <safascott78@hotmail.com>
2024-09-01 10:03:26 -0700

    Remove fbconfig

--- src/fbconfig.js
+++
@@ -1,9 +0,0 @@
-export default {
-    apiKey: "AIzaSyBgaw2u--TTZhel4q48FFvHSn9E9XBHrF0",
-  authDomain: "flaskapi-425105.firebaseapp.com",
-  projectId: "flaskapi-425105",
-  storageBucket: "flaskapi-425105.appspot.com",
-  messagingSenderId: "893621505924",
-  appId: "1:893621505924:web:99bb4a4634e1db8c7e17d1",
-  measurementId: "G-HC0LFWXLXW"
-  };
